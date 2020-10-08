2a7e918f3dfd5e5ba9fe431ecfcde6815c47bec4
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-08 18:15:56 -0300

    Configurando archivos estáticos

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express') //requiere que importe el paquete express instalado
 
-const server = express()
+const server = express() //el módulo express ejecutado se guarda en server
 
-server.listen(80)
+const docs = express.static("docs")
 
-server.get("/", function(request, response){  //en request va toda la información que envía la aplicación cliente -headers, body,
-    response.end("Hola mundo!")               //en response van todas los métodos y configuraciones para dar una respuesta al request
+server.use ( docs )
+
+server.listen(2000) //configuración del puerto
+
+server.get("/", function(request, response){  //en request va toda la información que envía la aplicación cliente -headers, body, en response van todas los métodos y configuraciones para dar una respuesta al request
+    
+    response.write("<h1>Titulo principal</h1>")
+    response.end("Hola clase desde NodeJs!")               
                                               //el .end va porque es la última operación que debe realizar la función y devolverle al navegador
-})                                            
+})
+
